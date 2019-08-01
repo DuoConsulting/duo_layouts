e564f1a0928003b5105d21c61f6ee0b65ae7088d
Michael Girgis <mgirgis@duoconsulting.com>
2019-08-01 16:21:11 -0500

    Add additional base layout styles

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/************************
+ * SETUP
+ ************************/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+/************************
+ * CONFIGURATION
+ ************************/
+
+var stylesSrc = [
+  './scss/layouts.scss'
+];
+
+/************************
+ * TASKS
+ ************************/
+
+gulp.task('styles', function() {
+  gulp.src(stylesSrc)
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: false
+    }))
+    // Catch any SCSS errors and prevent them from crashing gulp
+    .on('error', function (error) {
+      console.error(error);
+      this.emit('end');
+    })
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 11']
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles']);
